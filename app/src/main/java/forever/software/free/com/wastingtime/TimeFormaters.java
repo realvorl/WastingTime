666b6f148c044c81b25b7c802a8655401a3be048
Viorel PETCU <viorel.pe@gmail.com>
2017-03-11 21:57:49 +0100

    cleanup and resonable unit tests

--- app/src/main/java/forever/software/free/com/wastingtime/TimeFormaters.java
+++ app/src/main/java/forever/software/free/com/wastingtime/TimeFormaters.java
@@ -4,10 +4,10 @@ import java.text.SimpleDateFormat;
 import java.util.Locale;
 
 public class TimeFormaters {
-    final SimpleDateFormat timeFormatter = new SimpleDateFormat("HH'h'mm'm'ss's'", Locale.US);
+    public final SimpleDateFormat timeFormatter = new SimpleDateFormat("HH'h'mm'm'ss's'", Locale.US);
     public final SimpleDateFormat hrsFormatter = new SimpleDateFormat("HH'h'", Locale.US);
-    final SimpleDateFormat minFormatter = new SimpleDateFormat("mm'm'", Locale.US);
-    final SimpleDateFormat secFormatter = new SimpleDateFormat("ss's'", Locale.US);
+    public final SimpleDateFormat minFormatter = new SimpleDateFormat("mm'm'", Locale.US);
+    public final SimpleDateFormat secFormatter = new SimpleDateFormat("ss's'", Locale.US);
 
     public TimeFormaters() {
     }
--- app/src/test/java/forever/software/free/com/wastingtime/test/TimeFormatersTest.java
+++ app/src/test/java/forever/software/free/com/wastingtime/test/TimeFormatersTest.java
@@ -6,7 +6,8 @@ import java.util.Date;
 
 import forever.software.free.com.wastingtime.TimeFormaters;
 
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 
 public class TimeFormatersTest {
@@ -14,9 +15,30 @@ public class TimeFormatersTest {
     TimeFormaters classUnderTest = new TimeFormaters();
 
     @Test
-    public void testFormatOne() {
+    public void testFormatTime() {
+        Date testDate = new Date(123123123123123L);
+        String formatedDate = classUnderTest.timeFormatter.format(testDate);
+        assertThat(formatedDate, is("05h32m03s"));
+    }
+
+    @Test
+    public void testFormatHours() {
         Date testDate = new Date(123123123123123L);
         String formatedDate = classUnderTest.hrsFormatter.format(testDate);
-        assertTrue(formatedDate.equals(""));
+        assertThat(formatedDate, is("05h"));
+    }
+
+    @Test
+    public void testFormatMinute() {
+        Date testDate = new Date(123123123123123L);
+        String formatedDate = classUnderTest.minFormatter.format(testDate);
+        assertThat(formatedDate, is("32m"));
+    }
+
+    @Test
+    public void testFormatSeconds() {
+        Date testDate = new Date(123123123123123L);
+        String formatedDate = classUnderTest.secFormatter.format(testDate);
+        assertThat(formatedDate, is("03s"));
     }
 }
